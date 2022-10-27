13a0e76e1547cb2f56567b5c20312000615ed5a5
andytungsz <rainmay_service1@hotmail.com>
2022-10-27 11:03:26 +0800

    change to 1 channel

--- yolo.py
+++ yolo.py
@@ -328,8 +328,9 @@ class YOLO(object):
     def convert_to_onnx(self, simplify, model_path):
         import onnx
         self.generate(onnx=True)
-
+        # change  to 1 channel 
         im                  = torch.zeros(1, 3, *self.input_shape).to('cpu')  # image size(1, 3, 512, 512) BCHW
+        #im                  = torch.zeros(1, 3, *self.input_shape).to('cpu')  # image size(1, 3, 512, 512) BCHW
         input_layer_names   = ["images"]
         output_layer_names  = ["output"]
         
